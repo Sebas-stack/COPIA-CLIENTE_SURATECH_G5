72ee2f9521864e2ca86b2b7a3270df639afc0b17
Bernardo A Mira García <bamira1@misena.edu.co>
2024-10-28 11:24:28 -0500

    se reciben datos del formulario

--- src/controllers/controladorpaciente.js
+++ src/controllers/controladorpaciente.js
@@ -6,11 +6,44 @@
 //2. Por cada variable debo utilizar DOM para
 //asociarl el html con js
 let nombrePaciente=document.getElementById("nombrepaciente")
-let correoPaciente=document.getElementById("")
-let telefonoPaciente=document.getElementById("")
-let fechaNacimientoPaciente=document.getElementById("")
-let ciudadPaciente=document.getElementById("")
-let polizaPaciente=document.getElementById("ingresospaciente")
-let ipsPaciente=document.getElementById("")
-let grupoIngresoPaciente=document.getElementById("")
-let fechaAfiliacionPaciente=document.getElementById("")
+let correoPaciente=document.getElementById("correopaciente")
+let telefonoPaciente=document.getElementById("telefonopaciente")
+let fechaNacimientoPaciente=document.getElementById("nacimientopaciente")
+let ciudadPaciente=document.getElementById("ciudadpaciente")
+let polizaPaciente=document.getElementById("polizapaciente")
+let ipsPaciente=document.getElementById("ipspaciente")
+let grupoIngresoPaciente=document.getElementById("ingresospaciente")
+let fechaAfiliacionPaciente=document.getElementById("afiliacionpaciente")
+
+//3. Crear una variable para asociarla al id del boton que tengo en html
+let botonRegistroPaciente=document.getElementById("botonregistropaciente")
+
+//4. Detectamos acciones o eventos en el boton
+botonRegistroPaciente.addEventListener("click",function(evento){
+    evento.preventDefault()
+
+    //5. Se crea un JSON
+    //que capture todos los datos 
+    //del formulario
+    let datosFormularioPaciente={
+        nombres:nombrePaciente.value,
+        fechaNacimiento: fechaNacimientoPaciente.value,
+        ciudad:ciudadPaciente.value,
+        correo: correoPaciente.value,
+        telefono: telefonoPaciente.value,
+        ips: ipsPaciente.value,
+        grupoIngresos: grupoIngresoPaciente.value,
+        tienePoliza: polizaPaciente.value,
+        fechaAfiliacion: fechaAfiliacionPaciente.value
+        
+    }
+
+    //6. se envian los datos al back
+    console.log(datosFormularioPaciente)
+
+    Swal.fire({
+        title: "Registro Exitoso!",
+        text: "Ya eres parte de nuestra gran familia!",
+        icon: "success"
+    });
+})
